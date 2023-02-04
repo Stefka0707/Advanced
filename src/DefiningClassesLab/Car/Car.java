da97de364069a9343a331499ec74f29596808c56
Stefka0707 <stefka0707@gmail.com>
2023-02-04 17:19:35 +0200

    DefiningGlass

--- src/DefiningClassesLab/Car/Car.java
+++ src/DefiningClassesLab/Car/Car.java
@@ -1,15 +1,70 @@
 package DefiningClassesLab.Car;
 
+import java.util.Objects;
+
 public class Car {
     //we initialize fields
 
-   public String brand;
-    public String model;
-    public int horsePower;
+    //we initialize fields
 
-    public Car(String brandParam, String modelParam, int horsePowerParam) {
-        brand = brandParam;
-        model = modelParam;
-        horsePower = horsePowerParam;
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public  Car(String brand){
+        this(brand,"unknown",-1);
+
+    }
+    // Горния конструктое извиква долния
+    // но трябва да се запази сигнатурата
+    //а в горния ще получим само един параметър за вход
+    // сетваме останалите параметри с техните default стойности
+    public Car(String brand, String model, int horsePower) {
+        this. brand = brand;
+        this. model = model;
+        this. horsePower = horsePower;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The car is: %s %s - %d HP.",this.getBrand(),this.getModel(),getHorsePower());
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        DefiningClassesLab.Car.Car car = (DefiningClassesLab.Car.Car) o;
+//        return horsePower == car.horsePower && Objects.equals(brand, car.brand) && Objects.equals(model, car.model);
+//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, horsePower);
     }
 }
+
