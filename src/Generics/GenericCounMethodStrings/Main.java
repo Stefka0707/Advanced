69a03dd8f10cf15b1882b79e5180050acba2c526
Stefka0707 <stefka0707@gmail.com>
2023-02-10 17:04:15 +0200

    Genetic

---
+++ src/Generics/GenericCounMethodStrings/Main.java
@@ -0,0 +1,25 @@
+package Generics.GenericCounMethodStrings;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<String> box = new Box<String>();
+        for (int i = 0; i < n; i++) {
+            String value = scanner.nextLine();
+            box.addValue(value);
+        }
+
+        String toCompare = scanner.nextLine();
+
+        System.out.println(box.countGraterElements(toCompare));
+
+    }
+}
+
+
