417cccc49a63b86e7425f86b4c8bb8c2ad9b8fad
Stefka0707 <stefka0707@gmail.com>
2023-02-11 14:00:41 +0200

    ExamPreparation

---
+++ src/ExamPreparation9February2023/pastryShop.java
@@ -0,0 +1,68 @@
+package ExamPreparation9February2023;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class pastryShop {
+    public static final int BISCUIT_VALUE = 25;
+    public static final int CAKE_VALUE = 50;
+    public static final int PASTRY_VALUE = 75;
+    public static final int PIE_VALUE = 100;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    String [] liquidInfo = scanner.nextLine().split("\\s+");
+        Deque<Integer> liquids= new ArrayDeque<>();
+
+        for (String liquid : liquidInfo){
+            liquids.offer(Integer.parseInt(liquid));
+
+        }
+        String[] ingredientsInfo = scanner.nextLine().split("\\s+");
+
+        Deque<Integer> ingredients = new ArrayDeque<>();
+
+        for (String ingredient : ingredientsInfo) {
+            ingredients.push(Integer.parseInt(ingredient));
+        }
+
+        int biscuitCount =0,cakeCount =0,pastryCount =0,pieCount=0;
+
+    while (!liquids.isEmpty() && !ingredients.isEmpty()){
+        Integer liquid = liquids.poll();
+        Integer ingredient = ingredients.pop();
+        int possibleDish = liquid + ingredient;
+
+        switch (possibleDish){
+            case BISCUIT_VALUE : biscuitCount++;break;
+            case  CAKE_VALUE : cakeCount++;break;
+            case  PASTRY_VALUE:pastryCount++;break;
+            case PIE_VALUE:pieCount++;break;
+            default:ingredients.push(ingredient+3);break;
+
+        }
+    }
+    if (biscuitCount >0 && cakeCount >0 && pastryCount>0 && pieCount >0){
+        System.out.println("Great! You succeeded in cooking all the food!");
+    }else {
+        System.out.println("What a pity! You didn't have enough materials to cook everything.");
+    }
+
+        System.out.print("Liquids left: ");
+    if (liquids.isEmpty()){
+        System.out.println("none");
+    }else {
+System.out.println(liquids.toString().replaceAll("[\\[\\]]", ""));
+    }
+        System.out.print("Ingredients left: ");
+    if (ingredients.isEmpty()){
+        System.out.println("none");
+    }else {
+        System.out.println(String.join(", ", ingredients.toString()
+                  .replaceAll("[\\[\\]]", "")));
+    }
+        System.out.printf("Biscuit: %d\nCake: %d\nPie: %d\nPastry: %d\n",biscuitCount,cakeCount,pieCount,pastryCount);
+
+    }
+}
