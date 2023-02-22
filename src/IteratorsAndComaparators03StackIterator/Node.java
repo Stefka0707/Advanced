068152e5e6fb3a1cb8f10e7407c539ff3305819e
Stefka0707 <stefka0707@gmail.com>
2023-02-22 18:03:12 +0200

    IteratorsAndComparators

---
+++ src/IteratorsAndComaparators03StackIterator/Node.java
@@ -0,0 +1,17 @@
+package IteratorsAndComaparators03StackIterator;
+
+public class Node <Integer>{
+
+    public   int element;
+    public   Node<Integer> prev;
+
+
+
+        public Node(int element){
+            this.element = element;
+            this.prev = null;
+
+        }
+    }
+
+
